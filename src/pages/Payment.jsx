10daba1a87aa0c6915b27e2178145d3baa35401e
Haitham <haitham9559@gmail.com>
2025-10-04 02:19:24 -0900

    sdsdf

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -44,38 +44,38 @@ const Payment = () => {
     "/images/mastercard.png",
   ];
 
-  // const products = [
-  //   {
-  //     image: "https://i.imgur.com/DMQHGA0.jpeg",
-  //     name: "Mid-Century Modern Wooden Dining Table",
-  //     description:
-  //       "A beautiful wooden dining table with a mid-century modern design.",
-  //     price: "$299.99",
-  //   },
-  //   {
-  //     image: "https://i.imgur.com/6wkyyIN.jpeg",
-  //     name: "Modern Elegance Teal Armchair",
-  //     description:
-  //       "A stylish teal armchair that adds a touch of elegance to any room.",
-  //     price: "$99.99",
-  //   },
-  //   {
-  //     image: "https://i.imgur.com/3oXNBst.jpeg",
-  //     name: "Modern Minimalist Workstation Setup",
-  //     description:
-  //       "A sleek and modern workstation setup that enhances productivity.",
-  //     price: "$49.99",
-  //   },
-  //   {
-  //     image: "https://i.imgur.com/3dU0m72.jpeg",
-  //     name: "Modern Ergonomic Office Chair",
-  //     description:
-  //       "A comfortable and contemporary office chair designed for productivity.",
-  //     price: "$399.99",
-  //   },
-  // ];
+  const products = [
+    {
+      image: "https://i.imgur.com/DMQHGA0.jpeg",
+      name: "Mid-Century Modern Wooden Dining Table",
+      description:
+        "A beautiful wooden dining table with a mid-century modern design.",
+      price: "$299.99",
+    },
+    {
+      image: "https://i.imgur.com/6wkyyIN.jpeg",
+      name: "Modern Elegance Teal Armchair",
+      description:
+        "A stylish teal armchair that adds a touch of elegance to any room.",
+      price: "$99.99",
+    },
+    {
+      image: "https://i.imgur.com/3oXNBst.jpeg",
+      name: "Modern Minimalist Workstation Setup",
+      description:
+        "A sleek and modern workstation setup that enhances productivity.",
+      price: "$49.99",
+    },
+    {
+      image: "https://i.imgur.com/3dU0m72.jpeg",
+      name: "Modern Ergonomic Office Chair",
+      description:
+        "A comfortable and contemporary office chair designed for productivity.",
+      price: "$399.99",
+    },
+  ];
 
-  const products = paymentData?.items;
+  // const products = paymentData?.items;
 
   const [formData, setFormData] = useState({
     cardNumber: "",
